7917233f9d3e8d988114e720b64a057caa987b33
jeremy-jmc <jeremy.matos@utec.edu.pe>
2024-11-17 19:02:31 -0500

    API artists NodeJS: find by

--- backend/artist/index.mjs
+++ backend/artist/index.mjs
@@ -2,58 +2,49 @@ import AWS from "aws-sdk";
 AWS.config.update({ region: process.env.AWS_REGION });
 
 var ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+const TABLE_NAME = process.env.ARTIST_TABLE;
 
-var params = {
-    RequestItems: {
-        [process.env.ARTIST_TABLE]: {
-            Keys: [
-                
-            ]
-        }
-    }
-};
+/*
+Although it seems that this endpoint allows you to search by any field, it can only do so by artist_name
+*/
 
 export const handler = async (event) => {
-    // TODO implement
+    var params = {
+        TableName: TABLE_NAME,
+        KeyConditionExpression: "",
+        ExpressionAttributeValues: {
+        }
+    };
+    let json_request = event.body;
     let artist_colnames = ['artist_name', 'genres', 'subgenres', 'popularity', 'created_at'];
     let artist_colname_types = ['S', 'S', 'L', 'N', 'S'];
     artist_colnames.forEach((colname, index) => {
         const col_type = artist_colname_types[index];
 
-        if (event.hasOwnProperty(colname)) {
-            if (col_type == 'L') {
-                params.RequestItems[process.env.ARTIST_TABLE].Keys.push({
-                    [colname]: { L: event[colname].split(',') }
-                })
-            } else if (col_type == 'N') {
-                params.RequestItems[process.env.ARTIST_TABLE].Keys.push({
-                    [colname]: { N: event[colname] }
-                })
+        if (json_request.hasOwnProperty(colname)) {
+            params.KeyConditionExpression += `${colname} = :${colname} AND `;
+            if (col_type == 'N') {
+                params.ExpressionAttributeValues[`:${colname}`] = { N: json_request[colname] };
             } else if (col_type == 'S') {
-                params.RequestItems[process.env.ARTIST_TABLE].Keys.push({
-                    [colname]: { S: event[colname] }
-                })
+                params.ExpressionAttributeValues[`:${colname}`] = { S: json_request[colname] };
             }
         }
     });
+    params.KeyConditionExpression = params.KeyConditionExpression.slice(0, -5);
 
-    // let response_records = [];
-    let data = await ddb.batchGetItem(params).promise(); 
-    // data.Responses[process.env.ARTIST_TABLE].forEach(function (element, index, array) {
-    //     response_records.push(element);
-    // });
-
-    const response_records = data.Responses[process.env.ARTIST_TABLE].map(item => AWS.DynamoDB.Converter.unmarshall(item));
-
-    // ddb.batchGetItem(params, function (err, data) {
-    //     if (err) {
-    //         console.log("Error", err);
-    //     } else {
-    //         data.Responses[process.env.ARTIST_TABLE].forEach(function (element, index, array) {
-    //             response_records.push(element);
-    //         });
-    //     }
-    // });
+    let response_records = [];
+    try {
+        const data = await ddb.query(params).promise();
+        response_records = data.Items;
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: {
+                message: 'Error querying DynamoDB',
+                error: err
+            }
+        }
+    }
 
     const response = {
         statusCode: 200,
@@ -70,5 +61,11 @@ export const handler = async (event) => {
 };
 
 // https://docs.aws.amazon.com/es_es/sdk-for-javascript/v2/developer-guide/dynamodb-example-table-read-write-batch.html
+// TODO: https://stackoverflow.com/questions/31889891/validationexception-the-provided-key-element-does-not-match-the-schema
 
-
+/**
+ * Sample JSON request:
+ * {
+ *      "artist_name": "The Beatles"
+ * }
+ */
